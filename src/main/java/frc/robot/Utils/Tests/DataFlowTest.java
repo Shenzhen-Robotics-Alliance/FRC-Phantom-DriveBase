15b4bc0d423be6b14de837d8d524370e954fe99a
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2024-05-28 03:00:32 -0400

    the logic of physics simulation is a little wrong

--- src/main/java/frc/robot/Utils/Tests/DataFlowTest.java
+++ src/main/java/frc/robot/Utils/Tests/DataFlowTest.java
@@ -1,10 +1,12 @@
 package frc.robot.Utils.Tests;
 
+import edu.wpi.first.units.Angle;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Utils.EasyDataFlow;
+import frc.robot.Utils.MathUtils.AngleUtils;
+import frc.robot.Utils.MathUtils.LookUpTable;
 import frc.robot.Utils.MathUtils.Rotation2D;
 import frc.robot.Utils.MathUtils.Vector2D;
-import org.littletonrobotics.junction.Logger;
 
 public class DataFlowTest implements SimpleRobotTest {
     @Override
@@ -16,33 +18,40 @@ public class DataFlowTest implements SimpleRobotTest {
 
     Vector2D pos, vel;
     double rotation = 0;
-    final double maxVelocity = 10, maxAcceleration = 20, friction = 10, maxAngularVelocity = Math.toRadians(180);
+    final double maxVelocity = 5, maxAcceleration = 10, maxAccelerationAtFullSpeed = 4, friction = 8, maxAngularVelocity = Math.toRadians(180);
     long previousTimeMillis;
     @Override
     public void testPeriodic() {
         final XboxController xboxController = new XboxController(0);
         final double dt = (System.currentTimeMillis() - previousTimeMillis) / 1000.0f;
-        final Vector2D acc = new Vector2D(new double[] {xboxController.getLeftX(), -xboxController.getLeftY()}).multiplyBy((1-vel.getMagnitude()/maxVelocity) * maxAcceleration);
-        vel = vel.addBy(acc.multiplyBy(dt));
-        vel = new Vector2D(vel.getHeading(), Math.min(maxVelocity, vel.getMagnitude()));
-        final double velChangeDueToFriction=  friction * dt;
+        final double oldSpeed = vel.getMagnitude();
+        final Vector2D desiredMotion = new Vector2D(new double[] {xboxController.getLeftX(), -xboxController.getLeftY()}),
+                accByMotor = desiredMotion.multiplyBy(LookUpTable.linearInterpretation(0, maxAcceleration, maxVelocity, maxAccelerationAtFullSpeed, oldSpeed) + friction);
+
+        vel = vel.addBy(accByMotor.multiplyBy(dt));
+        final Vector2D centripetalForce = new Vector2D()
+        final double velChangeDueToFriction = friction * dt;
         if (vel.getMagnitude() < velChangeDueToFriction)
             vel = new Vector2D();
         else
             vel = new Vector2D(vel.getHeading(), vel.getMagnitude() - velChangeDueToFriction);
 
+        final double linearAccConstrain = LookUpTable.linearInterpretation(0, maxAcceleration, maxVelocity, 0, oldSpeed),
+                newSpeedConstrain = Math.min(maxVelocity, oldSpeed + linearAccConstrain * dt);
+        vel = new Vector2D(vel.getHeading(), Math.min(newSpeedConstrain, vel.getMagnitude()));
+
         pos = pos.addBy(vel.multiplyBy(dt));
         rotation += xboxController.getRightX() * dt * maxAngularVelocity;
         EasyDataFlow.putPosition("robot pos", pos, new Rotation2D(rotation));
 
-        EasyDataFlow.putSwerveState(
-                "chassis",
-                new Vector2D(new double[] {0,1}),
-                new Vector2D(new double[] {0,0}),
-                new Vector2D(new double[] {0,0}),
-                new Vector2D(new double[] {0,0}),
-                new Rotation2D(Math.toRadians(90))
-        );
+//        EasyDataFlow.putSwerveState(
+//                "chassis",
+//                new Vector2D(new double[] {0,1}),
+//                new Vector2D(new double[] {0,0}),
+//                new Vector2D(new double[] {0,0}),
+//                new Vector2D(new double[] {0,0}),
+//                new Rotation2D(Math.toRadians(90))
+//        );
 
 //        EasyDataFlow.putPositionArray(
 //                "position 1",
