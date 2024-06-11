691e75bd569cfeaa273ae2ed663e4277158a84f8
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2024-06-11 10:04:45 -0400

    added network alert display on dashboard from Team 6328

--- src/main/java/frc/robot/Utils/Tests/DashboardDataTest.java
+++ src/main/java/frc/robot/Utils/Tests/DashboardDataTest.java
@@ -1,20 +1,15 @@
 package frc.robot.Utils.Tests;
 
-import frc.robot.Modules.LEDStatusLights.SimulatedLEDStatusLight;
-import frc.robot.Utils.LEDAnimation;
+import frc.robot.Utils.Alert;
 
 public class DashboardDataTest implements SimpleRobotTest {
-    final SimulatedLEDStatusLight simulatedLEDStatusLight = new SimulatedLEDStatusLight(100);
     @Override
     public void testStart() {
-        simulatedLEDStatusLight.init();
-        simulatedLEDStatusLight.reset();
-        simulatedLEDStatusLight.enable();
+        final Alert alert = new Alert("Test Warning", Alert.AlertType.ERROR);
+        alert.set(true);
     }
 
     @Override
     public void testPeriodic() {
-        simulatedLEDStatusLight.setAnimation(new LEDAnimation.Slide(0,200, 255, 1, 0.8), null);
-        simulatedLEDStatusLight.periodic();
     }
 }
