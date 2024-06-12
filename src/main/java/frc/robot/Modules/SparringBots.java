0bc33437be4215a2764e243f87fb9821af48e6ea
lyr0621 <75480109+lyr0621@users.noreply.github.com>
2024-06-12 02:15:48 -0400

    added TODOs

---
+++ src/main/java/frc/robot/Modules/SparringBots.java
@@ -0,0 +1,23 @@
+package frc.robot.Modules;
+
+// TODO: write this class to simulate other robots on field
+public class SparringBots extends RobotModuleBase {
+    protected SparringBots() {
+        super("SparringBots", true);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    protected void periodic(double dt) {
+
+    }
+
+    @Override
+    public void onReset() {
+
+    }
+}
---
+++ src/main/java/frc/robot/Services/SparringBotsService.java
@@ -0,0 +1,28 @@
+package frc.robot.Services;
+
+// TODO: write this class to control sparring robots
+public class SparringBotsService extends RobotServiceBase {
+    protected SparringBotsService() {
+        super("Sparring-Bots");
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void periodic() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+}
